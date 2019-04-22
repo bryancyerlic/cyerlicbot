a7a3148aec8487b910040a677ce0497748178f9a
bryancyerlic <49858921+bryancyerlic@users.noreply.github.com>
2019-04-22 16:17:35 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,28 @@
-// Import the discord.js module
 const Discord = require('discord.js');
-
-// Create an instance of a Discord client
 const client = new Discord.Client();
 
-/**
- * The ready event is vital, it means that only _after_ this will your bot start reacting to information
- * received from Discord
- */
 client.on('ready', () => {
   console.log('I am ready!');
 });
 
-// Create an event listener for messages
 client.on('message', message => {  
-  //Start Function
-  
-  //001: Ping Pong
-  if (message.content === 'ping') {
-    // Send "pong" to the same channel
-    message.channel.send('pong');
-  }
-  //Next Function//
-  
+/**
+ * 001 Ping Pong
+ */
+ 
+	if (message.content === '!ping') {
+	// Send "pong" to the same channel
+	message.channel.send('pong');
+	}
+	
+/**
+ * 002 Avatar
+ */
+ 
+	if (message.content === '!avatar') {
+    message.reply(message.author.avatarURL);
+	
+	
+/********/	
 });
-
-// Log our bot in using the token from https://discordapp.com/developers/applications/me
 client.login(process.env.BOT_TOKEN);
