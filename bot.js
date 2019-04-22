160add552f81c1d7f1d1f1e1b9645a72ccfbd0ca
bryancyerlic <49858921+bryancyerlic@users.noreply.github.com>
2019-04-23 00:55:37 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,59 +14,20 @@ client.on('guildMemberAdd', member => {
   channel.send(`Welcome to the Cyerlic, ${member}`);
 });
 
-client.on('message', message => {
-  // Ignore messages that aren't from a guild
-  if (!message.guild) return;
-
-  // If the message content starts with "!kick"
-  if (message.content.startsWith('!kick')) {
-    // Assuming we mention someone in the message, this will return the user
-    // Read more about mentions over at https://discord.js.org/#/docs/main/stable/class/MessageMentions
-    const user = message.mentions.users.first();
-    // If we have a user mentioned
-    if (user) {
-      // Now we get the member from the user
-      const member = message.guild.member(user);
-      // If the member is in the guild
-      if (member) {
-        /**
-         * Kick the member
-         * Make sure you run this on a member, not a user!
-         * There are big differences between a user and a member
-         */
-        member.kick('Optional reason that will display in the audit logs').then(() => {
-          // We let the message author know we were able to kick the person
-          message.reply(`Successfully kicked ${user.tag}`);
-        }).catch(err => {
-          // An error happened
-          // This is generally due to the bot not being able to kick the member,
-          // either due to missing permissions or role hierarchy
-          message.reply('I was unable to kick the member');
-          // Log the error
-          console.error(err);
-        });
-      } else {
-        // The mentioned user isn't in this guild
-        message.reply('That user isn\'t in this guild!');
-      }
-    // Otherwise, if no user was mentioned
-    } else {
-      message.reply('You didn\'t mention the user to kick!');
-    }
-  }
-});
-
 client.on('message', message => {  
 /**
  * 001 Ping Pong
  */ 
-	if (message.content === '!ping') {
-		message.channel.send('pong');
-	}	
+	if(message.content === "!ping") {
+		// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
+		// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+		const m = await message.channel.send("Ping?");
+		m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
+	}
 /**
  * 002 Avatar
  */
-	if (message.content === '!avatar') {
+	if(message.content === '!avatar') {
 		message.reply(message.author.avatarURL);
 	}	
 /********/	
