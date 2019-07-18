1efbd5ea52639472a5c2f6e0162e78ca9fe26af2
Sou <vpvpgk@gmail.com>
2019-07-18 14:39:55 +0700

    add "nay" cmd

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const bossTimerChanelId = "600517067480891409";
 const nextBossRegexs = [/^boss\?$/, /^next\?$/];
 const bossAtRegexs = [/^at .*\?$/];
-const todayBossesRegexs = [/^today\?$/];
+const todayBossesRegexs = [/^today\?$/, /^nay\?$/];
 const nextdayBossesRegexs = [/^mai\?$/, /^tomorrow\?$/, /^nextday\?/];
 const checkInChanelId = "598426642976604170";
 const lobbyBdoChanelId= "419795241390833665";
